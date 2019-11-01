f22a8e712ef3429f370293eea03232a9fb107ee3
Myo Set Paing <myosetpaing@aceplussolutions.com>
2019-11-01 10:19:15 +0630

    project setup

---
+++ BuildingListAndNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildingListAndNavigation
+//
+//  Created by Myo Set Paing on 01/11/2019.
+//  Copyright © 2019 Myo Set Paing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
